d46e819b5a83b7e9c05424d44b02d83c91efe7da
lee <gosicklihai@tury.com>
2021-02-07 15:20:06 +0800

    add gamesoart

---
+++ src/main/Gamesoart.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Gamesoart {
+	public Pet[] petSoart;
+	
+	public Pet myPet;
+	
+	public Pet getMySoart() {
+		return myPet;
+	}
+
+//	public Pet[] getpetSoart() {
+////		return 
+//	}
+	
+	public void setMyPet(Pet myPet) {
+		this.myPet = myPet;
+	}
+	
+//	petSoart = new Pet{};
+}
